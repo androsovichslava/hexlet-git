574a2cf909abc42645876ca2afe36819077d8e32
androsovichslava <androsovichslava@gmail.com>
2023-10-01 22:47:51 +0300

    dd

--- capitalizeTests/tests/capitalize.test.js
+++ capitalizeTests/tests/capitalize.test.js
@@ -1,28 +1,5 @@
-import capitalize from '../src/capitalize.js'
 import { strict as assert } from 'node:assert';
-
-// import assert from 'power-assert';
-// if (capitalize('hello') !== 'Hello') {
-//     throw new Error('Функция работает неверно!');
-// }
-
-// if (capitalize('') !== '') {
-//     throw new Error('Функция работает неверно!');
-// }
-
-// assert.equal(capitalize(''), '');
-// Первый параметр actual – то, что пришло
-// Второй параметр expected – то, что ожидает тест
-// Правильный порядок аргументов имеет большое значение при анализе ошибки
-// assert.equal(capitalize('hello'), 'Hello');
-
-
-// assert.deepEqual({}, {}); // всё ок
-// assert.deepEqual({ key: 'value' }, { key: 'value' }); // всё ок
-// assert.deepEqual({ key: 'value' }, { key: 'another value' }); // Бум!
-
-// console.log('Все тесты пройдены!');
-
+// const assert = require('assert');
 const user = {
     name: 'Madonna',
     friends: ['Kate', 'Michel'],
